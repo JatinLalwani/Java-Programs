76d0e4539c34a718738b1388163def9e179bfd9b
JatinLalwani <lalwanijatin44@yahoo.com>
2016-09-03 23:07:23 +0530

    Add files via upload
    A simple program which takes two integers and conclude whether or not the numbers are coprime.

---
+++ Coprime.java
@@ -0,0 +1,39 @@
+// Coprime numbers are the numbers whose GCD/HCF = 1.
+import java.util.Scanner;
+class Coprime{
+	public static void main(String... args){
+		Scanner s = new Scanner(System.in);
+		System.out.println();
+		System.out.println("Coprime numbers are the numbers whose GCD/HCF = 1.");
+		System.out.println();
+		System.out.println("Enter two integers-");
+		System.out.println();
+		System.out.print("First integer: ");
+		int r1 = s.nextInt();
+		System.out.print("Second integer: ");
+		int r2 = s.nextInt();
+		
+		if(r1<r2){
+			r1 = r1+r2; // A simple way
+			r2 = r1-r2; // to exchange
+			r1 = r1-r2; // numbers without temp variable ;).
+		}
+		
+		System.out.println();
+		System.out.print("Numbers "+r1+" and "+r2+" are ");
+		
+		while((r1%r2)!= 0){
+			int a = r1;
+			r1 = r2;
+			r2 = a%r2;
+		}
+		
+		if(r2 == 1){
+			System.out.print("Coprime!");
+		} else{
+			System.out.print("not Coprime.");
+		}
+		
+		System.out.println();
+	}
+}
