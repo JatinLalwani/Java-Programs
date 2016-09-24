0434161cb6f134d95cb1799cfe76505e719e9255
JatinLalwani <JatinLalwani@users.noreply.github.com>
2016-09-24 18:48:58 +0530

    Add files via upload

---
+++ LogicPattern2.java
@@ -0,0 +1,28 @@
+/*    +
+     + +
+	+ + +
+     - -
+      +
+    + + +
+*/
+	
+class LogicPattern2{
+	public static void main(String... args){
+		for(int i=0;i<3;i++){ // first 3 rows.
+			for(int j=2-i;j>=0;j--)  // for spaces
+				System.out.print(" ");
+			
+		    for(int k=0;k<i+1;k++) // for first three + rows
+			    System.out.print("+ ");
+		    System.out.println();
+		}
+		System.out.print("  - - \n");
+		for(int i=0;i<3;i+=2){ // for last 2 lines
+			for(int j=2-i;j>=0;j--)
+				System.out.print(" ");
+			for(int k=0;k<=i;k++)
+				System.out.print("+ ");
+			System.out.println();
+		}			
+	}
+}
