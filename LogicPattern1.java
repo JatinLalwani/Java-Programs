082cdc44382b46795007ea1cbba5d25448f336ff
JatinLalwani <JatinLalwani@users.noreply.github.com>
2016-09-24 18:48:41 +0530

    Add files via upload

---
+++ LogicPattern1.java
@@ -0,0 +1,52 @@
+/*       *
+        * *
+	   *   *
+	  *     *
+	   *   *
+	    * *
+		 *
+*/
+		 
+class LogicPattern1{
+	public static void main(String... args){
+		int[][] a = new int[7][7];
+		
+		for(int i=0;i<7;i++){
+			
+			for (int j=0;j<7;j++){
+				if(i<4){
+					if(j<4){
+						if((i+j)==3){
+							System.out.print("*");
+						} else{
+							System.out.print(" ");
+						}
+					} else{
+						if((j-i)==3){
+							System.out.print("*");
+						} else{
+							System.out.print(" ");
+						}
+					}
+				} else{
+					if(j<4){
+						if((i-j)==3){
+							System.out.print("*");
+						} else{
+							System.out.print(" ");
+						}
+					} else{
+						if((i+j)==9){
+							System.out.print("*");
+						} else{
+							System.out.print(" ");
+						}
+					}
+				}
+				
+			}
+			
+			System.out.println();
+		}
+	}
+}
