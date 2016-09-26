c365be1d983f7c3f46ee350c254da5f50ec76e1b
JatinLalwani <JatinLalwani@users.noreply.github.com>
2016-09-26 18:05:58 +0530

    Update LogicPattern4.java

--- LogicPattern4.java
+++ LogicPattern4.java
@@ -13,19 +13,21 @@
 
 */
 
+// Logic is, for a row Print required number of Stars then print space(if needed) and then again print stars.
+
 class LogicPattern4{
 	public static void main(String... args){
-		for(int i=0;i<5;i++){
+		for(int i=0;i<5;i++){ // first five rows.
 			for(int j=5-i;j>0;j--)
-				System.out.print("*");
+				System.out.print("*"); 
 			for(int k=0;k<i;k++)
-				System.out.print("  ");
+				System.out.print("  "); // Double space.
 			for(int j=5-i;j>0;j--)
 				System.out.print("*");
 			System.out.println();
 		}
 		
-		for(int i=0;i<5;i++){
+		for(int i=0;i<5;i++){ // Next five rows.
 			for(int j=i;j>=0;j--)
 				System.out.print("*");
 			for(int k=4-i;k>0;k--)
